73930e743ba8cd96ba031197b05379246e0b67de
Jigisha-Ponda <jigishaadatiya21@gmail.com>
2025-06-25 22:03:18 +0530

    added projects with project details

--- package/src/components/Portfolio/PortfolioList/index.tsx
+++ package/src/components/Portfolio/PortfolioList/index.tsx
@@ -4,24 +4,26 @@ import { Portfolio } from "@/types/blog";
 import PortfolioCard from "../PortfolioCard";
 
 const PortfolioList: React.FC = () => {
-  const portfolios: Portfolio[] = getAllPortfolios([
-    "title",
-    "date",
-    "type",
-    "excerpt",
-    "coverImage",
-    "slug",
-  ]);
+    const portfolios: Portfolio[] = getAllPortfolios([
+        "title",
+        "date",
+        "type",
+        "excerpt",
+        "coverImage",
+        "slug",
+    ]);
 
-  return (
-    <section id="portfolio" className="pt-20 pb-24">
-      <div className="container mx-auto grid md:grid-cols-2 gap-12">
-        {portfolios.map((portfolio, i) => (
-          <PortfolioCard key={i} portfolio={portfolio} />
-        ))}
-      </div>
-    </section>
-  );
+    return (
+        <section id="portfolio" className="pt-20 pb-24">
+            <div className="container mx-auto px-4 ">
+                <div className="grid md:grid-cols-2 gap-12">
+                    {portfolios.map((portfolio, i) => (
+                        <PortfolioCard key={i} portfolio={portfolio} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default PortfolioList;
